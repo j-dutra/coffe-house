f1d7811bdb90865f2a96ff34257d4fd533e44175
João L. Dutra <jdutra.eu@gmail.com>
2023-04-21 23:44:09 -0300

    add style data section

--- script.js
+++ script.js
@@ -11,3 +11,32 @@ const swiper = new Swiper('.swiper', {
     clickable: true,
   },
 })
+
+let menu = document.querySelector('.menu')
+let nums = document.querySelectorAll('.num')
+let start = false
+
+window.addEventListener('scroll', () => {
+  const navbar = document.querySelector('.navbar')
+
+  navbar.classList.toggle('sticky', window.scrollY > 0)
+
+  if (window.scrollY >= menu.offsetTop) {
+    if(!start) {
+      nums.forEach(num => {
+        startCount(num)
+      })
+    }
+    start = true
+  }
+})
+
+const startCount = (el) => {
+  let max = el.dataset.val
+  let count = setInterval(() => {
+    el.textContent++
+    if (el.textContent === max) { 
+      clearInterval(count)
+    }
+  }, 2000 / nums)
+}
